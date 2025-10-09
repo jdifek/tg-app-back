2b4cfef0cbc423529d518cacc95f5bb083d0d778
jdifek <givotkov11@gmail.com>
2025-10-09 10:44:10 +0300

    wqwq

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const prisma = new PrismaClient();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+app.set('trust proxy', 1); 
 // Middleware
 app.use(helmet());
 app.use(cors());
